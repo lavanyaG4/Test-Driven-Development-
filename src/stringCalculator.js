ec53cffb70544c53d82c9a93026ddac0b8a87e7f
lavanyagunnam <lavanya.g@gradright.com>
2024-10-16 14:51:04 +0530

    Feature: Handle single number string input.

---
+++ src/stringCalculator.js
@@ -0,0 +1,11 @@
+function add(numbers) {
+    if (numbers === "") {
+      return 0;
+    }
+    return parseInt(numbers);
+    
+}
+  
+module.exports = {
+    add
+}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 const stringCalculator = require('../src/stringCalculator');
 
 describe('String Calculator', () => {
-    test('should return 0for an empty string', () => {
-        expect(stringCalculator('')).toBe(0);
+    test('should return 0 for an empty string', () => {
+        expect(stringCalculator.add("")).toBe(0);
     });
+
+    test('should return sum of numbers for given numbers', () => {
+        expect(stringCalculator.add("1")).toBe(1);
+    })
+   
 });
